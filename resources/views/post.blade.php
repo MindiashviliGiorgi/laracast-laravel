8cbd9e26cdf25d47d081f9bee4a14f77470a1618
MindiashviliGiorgi <mindiashvilife@gmail.com>
2024-01-03 21:32:58 +0400

    go

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gm Laracast</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1>My First Post</h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptates architecto placeat, dolores minima ea perspiciatis in voluptas repudiandae enim facilis, blanditiis molestiae nisi amet fugit rem? Nemo, soluta repellat.</p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gm Laracast</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptates architecto placeat, dolores minima ea perspiciatis in voluptas repudiandae enim facilis, blanditiis molestiae nisi amet fugit rem? Nemo, soluta repellat.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptates architecto placeat, dolores minima ea perspiciatis in voluptas repudiandae enim facilis, blanditiis molestiae nisi amet fugit rem? Nemo, soluta repellat.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptates architecto placeat, dolores minima ea perspiciatis in voluptas repudiandae enim facilis, blanditiis molestiae nisi amet fugit rem? Nemo, soluta repellat.</p>
+    </article>
+</body>
+</html>
